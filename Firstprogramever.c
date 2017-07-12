4bc263aa99e11542d2286d2f834e1bd5d2d9b1a1
Steve Taylor <snktaylor@bellsouth.net>
2017-07-11 21:11:20 -0400

    First commit

---
+++ Firstprogramever.c
@@ -0,0 +1,43 @@
+/*
+*create a vector
+*/
+
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void printVector(vector<int> b)
+{
+
+	cout << "|";	
+
+
+	for(int = 0; i < b.size(); i++)
+	{
+
+	cout << b[i] << "|"; 
+
+	}
+
+}
+
+int main()
+
+{
+	vector<int> v(3);
+
+	v[0] = 10;
+	v[1] = 22;
+	v[2] = 5;
+
+	v.push_back(7);
+
+	cout << v.front() << endl;
+	cout << v.back() << endl;
+	cout << v.at(2) << endl;
+
+	return 0;
+
+}
